8c1ce51dc79a1bae4a21f64c5cd67a9ddc10fdf9
Val Catalasan <val@bscmanage.com>
2016-02-04 14:26:30 -0800

    send member profile update to external directory service

--- includes/bsc-membership-search.php
+++ includes/bsc-membership-search.php
@@ -25,8 +25,63 @@ class BSC_Membership_Search
     {
         add_action('xprofile_fields_saved_field', array($this, 'export_membership_table'));
         add_action('xprofile_fields_deleted_field', array($this, 'export_membership_table'));
+        add_action('xprofile_updated_profile', array($this, 'xprofile_updated_profile'), 10, 5);
+
+
+        add_filter('bps_request_data', array($this, 'search_form'), 10, 1);
     }
 
+    function xprofile_updated_profile($user_id, $field_ids, $errors, $old_values, $new_values) {
+        $fields = $this->bp_get_profile_fields();
+        $update = array(
+            'user_id' => $user_id
+        );
+        foreach ($field_ids as $field_id) {
+            $update[ $fields[ $field_id ]['name'] ] = $new_values[ $field_id ]['value'];
+        }
+
+        $this->sendPost('http://10.10.10.47:3000/api/profiles', $update);
+    }
+
+    // use PHP streams API to send data
+    function sendPost($server, $data) {
+        $data = json_encode($data);
+
+        $result = file_get_contents($server, null, stream_context_create(array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-Type: application/json' . "\r\n"
+                            . 'Content-Length: ' . strlen($data) . "\r\n",
+                'content' => $data,
+            ),
+        )));
+        return $result;
+    }
+
+    // use curl to send data
+    function sendPost2($server, $data) {
+        $data = json_encode($data);
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $server);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+        );
+
+        $result = curl_exec ($ch);
+        curl_close ($ch);
+        return $result;
+    }
+
+    function search_form($content) {
+        error_log(print_r($content,true));
+        return $content;
+    }
     function register_scripts_stylesheets()
     {
         add_action('admin_enqueue_scripts', array($this, 'custom_scripts'));
@@ -68,8 +123,8 @@ class BSC_Membership_Search
 
         // CREATE NEW VIEW
         $query = "CREATE VIEW $table_name AS SELECT a.user_id, b.user_status, b.user_login, b.user_email";
-        foreach ($fields as $field_id => $field_name) {
-            $query .= sprintf(", GROUP_CONCAT(if(field_id=%d,value,NULL)) as '%.64s'", $field_id, $field_name);
+        foreach ($fields as $id => $field) {
+            $query .= sprintf(", GROUP_CONCAT(if(field_id=%d,value,NULL)) as '%.64s'", $id, $field['name']);
         }
         $query .= sprintf(' FROM %s AS a LEFT JOIN %s AS b ON (a.user_id = b.ID) GROUP BY user_id', $bp->profile->table_name_data, $wpdb->prefix . 'users');
         return $wpdb->query($query);
@@ -82,10 +137,10 @@ class BSC_Membership_Search
         $fields = array();
 
         if ($bp) {
-            $query = "SELECT id, name FROM {$bp->profile->table_name_fields} WHERE parent_id = 0";
+            $query = "SELECT id, name, type FROM {$bp->profile->table_name_fields} WHERE parent_id = 0";
             $records = $wpdb->get_results($query);
             foreach ($records as $record)
-                $fields[$record->id] = $this->camel_case($record->name);
+                $fields[$record->id] = array('name' => $this->camel_case($record->name), 'type' => $record->type);
         }
         return $fields;
     }
